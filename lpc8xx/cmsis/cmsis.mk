1d0d400da1bc65d88eba11db561c6e9c80821d08
Tonny Tzeng <tonny.tzeng@gmail.com>
2020-12-19 15:10:41 +0800

    lpc8xx: fix the regression of conditional check of new CMSIS drivers inclusion

--- lpc8xx/cmsis/cmsis.mk
+++ lpc8xx/cmsis/cmsis.mk
@@ -2,7 +2,7 @@ d      := $(where-am-i)
 module := $(notdir $d)
 libs-y := $(libs-y) $(module)
 
-drivers         := $(if $(CONFIG_CMSIS_CODE_BUNDLE:y=),drivers,peripherals utilities)
+drivers         := $(if $(findstring y,$(CONFIG_CMSIS_CODE_BUNDLE)),peripherals utilities,drivers)
 src-$(module)-y := $(wildcard $(foreach s,core $(drivers),$d/$s/src/*.c))
 defines-y       += __USE_CMSIS=CMSIS_CORE_LPC8xx
 includes-y      := $(includes-y) $(wildcard $(foreach s,core $(drivers),$d/$s/inc))
