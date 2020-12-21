1023c30ee272d6aaba09da8a1830ccc369e11c8e
Tonny Tzeng <tonny.tzeng@gmail.com>
2020-12-21 15:41:16 +0800

    CMSIS-RTX: upgrade to CMSIS Verson 5 and enable building from source
    Fixed #2

--- external/CMSIS-RTX.mk
+++ external/CMSIS-RTX.mk
@@ -1,9 +1,22 @@
 # The CMSIS-RTX module is part of the CMSIS GitHub repository.
-# The module is set up by:
+# The module is set up to build against CMSIS Version 4 by:
 #	git clone https://github.com/ARM-software/CMSIS.git
+# or CMSIS Version 5 by:
+#	git clone https://github.com/ARM-software/CMSIS_5.git
 d      := $(where-am-i)
 module := $(who-am-i)
 
-d          := $d/CMSIS/CMSIS/RTOS/RTX
-includes-y := $(includes-y) $d/INC
-ldflags-y  += -L $d/LIB/GCC
+cmsis      := $d/CMSIS_5/CMSIS
+rtos       := $(cmsis)/RTOS/RTX
+includes-y := $(includes-y) $(rtos)/INC
+
+ifdef CONFIG_CMSIS_ARCH
+libs-y := $(libs-y) $(module)
+
+src-$(module)-y := $(rtos)/SRC/GCC/SVC_Table.S \
+                   $(rtos)/SRC/GCC/HAL_C$(CONFIG_CMSIS_ARCH).s \
+                   $(wildcard $(rtos)/SRC/*.c)
+$(eval $(call add-local-cflags,$(rtos)/SRC/rt_CMSIS.c,-I $(cmsis)/Include -I $(cmsis)/Core/Include))
+else
+ldflags-y += -L $(rtos)/LIB/GCC
+endif
