fe6245316bf199cce1fabc5ec42161ea413ddc0c
Tonny Tzeng <tonny.tzeng@gmail.com>
2021-06-01 14:39:18 +0800

    lpcopen: supports NO_BOARD_LIB compile option for chip only applications

--- lpc175x_6x/lpcopen/lpcopen.mk
+++ lpc175x_6x/lpcopen/lpcopen.mk
@@ -2,11 +2,16 @@ d      := $(where-am-i)
 module := $(notdir $d)
 libs-y := $(libs-y) $(module)
 
-src-$(module)-y := $(wildcard $d/src/*.c \
-                              $d/board/$(CONFIG_BOARD)/src/*.c)
+src-$(module)-y := $(wildcard $d/src/*.c)
 defines-y       += __USE_LPCOPEN CORE_M3
-includes-y      := $(includes-y) $d/inc \
-                                 $d/board/$(CONFIG_BOARD)/inc
+includes-y      := $(includes-y) $d/inc
+
+ifneq ($(CONFIG_BOARD),)
+src-$(module)-y += $(wildcard $d/board/$(CONFIG_BOARD)/src/*.c)
+includes-y      := $(includes-y) $d/board/$(CONFIG_BOARD)/inc
+else
+defines-y       += NO_BOARD_LIB
+endif
 
 # Use USBD LIB stack
 includes-$(CONFIG_LPCOPEN_USBD) += $d/inc/usbd
--- lpc8xx/lpcopen/lpcopen.mk
+++ lpc8xx/lpcopen/lpcopen.mk
@@ -2,11 +2,16 @@ d      := $(where-am-i)
 module := $(notdir $d)
 libs-y := $(libs-y) $(module)
 
-src-$(module)-y := $(wildcard $d/src/*.c \
-                              $d/board/$(CONFIG_BOARD)/src/*.c)
+src-$(module)-y := $(wildcard $d/src/*.c)
 defines-y       += __USE_LPCOPEN CORE_M0PLUS
-includes-y      := $(includes-y) $d/inc \
-                                 $d/board/$(CONFIG_BOARD)/inc
+includes-y      := $(includes-y) $d/inc
+
+ifneq ($(CONFIG_BOARD),)
+src-$(module)-y += $(wildcard $d/board/$(CONFIG_BOARD)/src/*.c)
+includes-y      := $(includes-y) $d/board/$(CONFIG_BOARD)/inc
+else
+defines-y       += NO_BOARD_LIB
+endif
 
 # Suppress compiler warnings in iap.c
 $(eval $(call add-local-cflags,iap.c,-w))
